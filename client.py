171cb58599524021f8c0d88c0900f5aa4f7086e0
Bandi <totthbandus10@gmail.com>
2021-04-30 18:56:55 +0200

    Keretezes TESZTELETLEN

--- client.py
+++ client.py
@@ -2,40 +2,67 @@ import services
 from config import data as config_data
 import threading
 from service_states import UserState
+import hashlib
+import Crypto
 
 #Ide jön a kliensoldali parancsok lekezelése
 #Visszaadja a szervernek küldendő adatot
-def orders(user_state):
+def orders():
     data=""
     input_string=""
     #Ha nincs a user bejelentkezve, akkor elkérjük a jelszót
-    if user_state != UserState.LOGED_IN:
-        print("You have to log in! Enter your password!")
-        input_string=input()
-        data=input_string
+    print("Enter your command!")
+    input_string = input()
+    command = input_string.split(' ')
+
+    if command[0] == ["MKD", "RMD","GWD","CWD","LST","UPL","DNL","RMF"]:
+        command_string = ",".join([command[0],command[1],str(actuals.order_count),str(services.current_time_milis())])
+        h_obj = Crypto.Hash.SHA3_256.new()
+        h_obj.update(command_string)
+        return  ",".join(command_string,h_obj.hexdigest())
     else:
-        print("Enter your command!")
-        input_string = input()
+        print("Invalid command")
+        return None
 
-    return data
 
+def login():
+    print("You have to log in! Enter your password!")
+    password=input()
+    actuals.AES_key = Crypto.Random.get_random_bytes(32)
+    actuals.order_count = 0
+    message = ",".join(password,actuals.AES_key,actuals.order_count, services.current_time_milis())
+    h_obj = Crypto.Hash.SHA3_256.new()
+    h_obj.update(message)
+    message_with_hash = ",".join(message,h_obj.hexdigest())
+    #TODO: MÁTÉ RSA ELKÜLDÉSE
+
+
+
+
+class actuals:
+    AES_key=None
+    order_count = None
 
 def main():
     data=""
     user_state= UserState.NOT_LOGED_IN
+    login()
+
     while order != "Exit":
-        order = orders(user_state)
+        actuals.command_count+=1
+        order = orders()
+        if order is not None:
+            #Válaszüzenetre váró szál indítása
+            answer_cathcing_thread = threading.Thread(target=services.listening, args=(config_data.client_port,))
+            answer_cathcing_thread.start()
 
-        #Válaszüzenetre váró szál indítása
-        answer_cathcing_thread = threading.Thread(target=services.listening, args=(config_data.client_port,))
-        answer_cathcing_thread.start()
+
+            #Adatok elküldése a szervernek
+            services.senddata(config_data.localhost,config_data.server_port)
 
 
-        #Adatok elküldése a szervernek
-        services.senddata(config_data.localhost,config_data.server_port)
-
-        #Megvárjuk a választ
-        answer_cathcing_thread.join()
+            #Megvárjuk a választ
+            answer_cathcing_thread.join()
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -1,4 +1,8 @@
+import hashlib
 import shutil
+
+import Crypto
+
 import services
 from config import data as config_data
 import os
@@ -6,7 +10,8 @@ from os.path import exists as exists
 import pathlib
 
 
-def make_folser(folder_path):
+
+def make_folder(folder_path):
     if not exists(folder_path):
         os.makedirs(folder_path)
 
@@ -15,20 +20,125 @@ def delete_folder(folder_path):
         shutil.rmtree(folder_path)
 
 def source_directory():
-    return pathlib.Path.absolute()
+    return os.getcwd()
 
 def list_directory(path):
     return os.listdir(path)
 
+def has_acces_to_file(user, path):
+    if source_directory()+"/"+actuals.user in path:
+        return True
+    return False
+
+class actuals:
+    user = None
+    path = source_directory()
+    roote_path = source_directory()
+    last_order_count = None
+    AES_key = None
+
+
+def to_directory(path):
+    if has_acces_to_file(path):
+        actuals.path = path
+
+
+
+
+def order_parse_and_doit(order):
+    #Hash levétel és ellenőrzés
+    message=order.split(",")
+    hash = message.pop()
+    h_obj = Crypto.Hash.SHA3_256.new()
+    h_obj.update(",".join(message))
+    if h_obj.hexdigest() != hash:
+        return "Error: Hash"
+
+    #Timestamp ellenőrzés
+    timestamp = int(message.pop())
+    if timestamp + 10*1000 < services.current_time_milis():
+        return "Error: To old message"
+
+    order_count = int(message.pop())
+    if order_count != actuals.last_order_count+1:
+        return "Error: Not the next order"
+    else:
+        actuals.last_order_count = order_count
+
+        if message[0] == "MKD":
+            make_folder(actuals.path+"/"+order[1])
+            return "Done"
+        if message[0] == "RMD":
+            delete_folder(actuals.path+"/"+order[1])
+            return "Done"
+        if message[0] == "GWD":
+            return actuals.path
+        if message[0] == "CWD":
+            print()
+            #TODO
+        if message[0] == "LST":
+            return "List,"+",".join(list_directory(actuals.path))
+        if message[0] == "UPL":
+            #TODO: lecserélni szabi függvényére
+            services.listening(1)
+            return "Done"
+        if message[0] == "DNL":
+            #TODO: lecserélni szabi függvényére
+            services.senddata(1,1)
+        if message[0] == "RMF":
+            delete_folder(actuals.path+"/"+order[1])
+        else:
+            return "Unknown command"
+
+
+def login():
+    while True:
+        #TODO: Szabi függcénye várakozik
+        login_message=""
+
+        message=login_message.split(",")
+        hash = message.pop()
+        h_obj = Crypto.Hash.SHA3_256.new()
+        h_obj.update(",".join(message))
+        if h_obj.hexdigest() != hash:
+            print()
+            #TODO: Szabi String üzenet: HASH ERROR
+
+        #Timestamp ellenőrzés
+        timestamp = int(message.pop())
+        if timestamp + 10*1000 < services.current_time_milis():
+            #TODO: Szabi
+            return "Error: To old message"
+
+        order_count = int(message.pop())
+        if order_count != 0:
+            #TODO szabi
+            return "Error: Not the next order"
+        actuals.last_order_count = 0
+
+        AES_key = message.pop()
+        if len(AES_key) != 32:
+            print()
+            #TODO: Szabi serror
+
+        h_obj = Crypto.Hash.SHA3_256.new()
+        h_obj.update(",".join(message))
+        actuals.user = h_obj.hexdigest()
+        to_directory(actuals.user)
+
+
 
 def main():
+    login()
     while True:
-        data = services.listening(config_data.server_port)
+        if actuals.user == None:
+            login()
+        order = services.listening(config_data.server_port)
         #adat feldolgozása
 
-        print("Datasend")
-        print(services.current_time_milis())
-        answer=""
+        answre=order_parse_and_doit(order)
+
+        #TODO: SZABI STRING KÜLDŐJE
         services.senddata(config_data.localhost,config_data.client_port)
 
 if __name__ == "__main__":
